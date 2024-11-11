edc6d1b3579666afe55ed6abe15768f58aeed358
mirellaguiar <168469751+mirellaguiar@users.noreply.github.com>
2024-11-11 17:12:26 -0300

    importando arquivos

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Língua Portuguesa',
+    'O que é uma oração subordinada?',
+    'É uma oração que depende de outra para ter sentido completo'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é uma célula?',
+    'Unidade básica da vida, responsável por funções essenciais para o funcionamento dos seres vivos'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é o valor de π?',
+    'Aproximadamente 3,14'
+)
